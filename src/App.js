fac9167920e4aa81515a461447b294346070544a
Manvendra Singh <mjdeora@gmail.com>
2020-02-24 22:49:24 +0530

    Basic fucntionality complete

--- src/App.js
+++ src/App.js
@@ -8,6 +8,23 @@ class App extends Component {
     movies: []
   }
 
+  // Debounce / Throttling
+  // Reponsive
+
+  async componentDidUpdate(prevProps, prevState) {
+    if (prevState.title !== this.state.title && this.state.title.length > 2) {
+      let response = await fetch(`https://www.omdbapi.com/?apikey=45af4549&page=1&s=${this.state.title}`)
+
+      response = await response.json()
+
+      let movies = response.Search || []
+
+      movies = movies.slice(0, 5)
+
+      this.setState({ movies })
+    }
+  }
+
   render() {
     const { title, movies } = this.state
 
@@ -16,7 +33,12 @@ class App extends Component {
         <div className="app-container">
           <input type="text" placeholder="Search movies..." className="search-bar" value={title} onChange={e => this.setState({ title: e.target.value })} />
           <div>
-            {movies.map(movie => movie)}
+            {movies.map(movie => {
+              return <div key={movie.imdbID}>
+                <h3>{movie.Title}</h3>
+                <img src={movie.Poster} alt="movie" />
+              </div>
+            })}
           </div>
         </div>
       </div>
